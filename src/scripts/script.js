0a0f13a0262c4e9b130fdc59eadffd1b24f9565c
Navi026 <karlos123jun@gmail.com>
2022-05-19 20:16:21 -0500

    feat: RFC added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,6 +5,7 @@ function addarray() {
     let lastname1 = document.getElementById('lastname1').value;
     let lastname2 = document.getElementById('lastname2').value;
     let namecoomplete = name + lastname1 + lastname2;
-    register.push(namecoomplete)
+    let RFC =lastname1.substring(0,2) + lastname2.substring(0,1) + name.substring(0,1)
+    register.push(namecoomplete,RFC)
     console.log(register)
 }
