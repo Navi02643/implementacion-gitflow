fa25705f8d6f334e0231d523d3b28f476db1ab04
Navi026 <karlos123jun@gmail.com>
2022-05-19 19:28:10 -0500

    feat: finished form function

---
+++ src/scripts/script.js
@@ -0,0 +1,10 @@
+let register = [];
+
+function addarray() {
+    let name = document.getElementById('name').value;
+    let lastname1 = document.getElementById('lastname1').value;
+    let lastname2 = document.getElementById('lastname2').value;
+    let namecoomplete = name + lastname1 + lastname2;
+    register.push(namecoomplete)
+    console.log(register)
+}
