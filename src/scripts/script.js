83c50126360bc777c15c339dcae027e165664af6
Navi026 <karlos123jun@gmail.com>
2022-05-19 19:41:00 -0500

    fix: name are now displayed separately

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,7 +4,7 @@ function addarray() {
     let name = document.getElementById('name').value;
     let lastname1 = document.getElementById('lastname1').value;
     let lastname2 = document.getElementById('lastname2').value;
-    let namecoomplete = name + lastname1 + lastname2;
+    let namecoomplete = name + " " + lastname1 + " " + lastname2;
     register.push(namecoomplete)
     console.log(register)
 }
